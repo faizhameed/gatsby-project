13d45535e5d9fd76aafb0a49a4b1297ff11fae48
faizhameed <faizftb@gmail.com>
2020-03-08 11:08:41 +0530

    created slug pointing to the template pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,3 +14,43 @@ module.exports.onCreateNode = ({ node, actions }) => {
     })
   }
 }
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/blog.jsx")
+  //1. get path to templates
+  //2. get markdown data
+  //3. Create new pages
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `) // this graphql function returns a promise
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      // Path for this page — required
+      path: `/blog/${edge.node.fields.slug}`,
+      component: blogTemplate,
+      context: {
+        // Add optional context data to be inserted
+        // as props into the page component..
+        //
+        // The context data can also be used as
+        // arguments to the page GraphQL query.
+        //
+        // The page "path" is always available as a GraphQL
+        // argument.
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
+}
